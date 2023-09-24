5af00b34c904634858884f747a0892c1f1919644
Anthony Hsu <129446589+anthony-hsu@users.noreply.github.com>
2023-09-24 15:41:41 -0700

    Add finished components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-import './App.css'
+import { useState, useEffect } from "react";
+import Board from "./components/Board";
+import Header from "./components/Header";
+import "./App.css";
 
 function App() {
+  const [seen, setSeen] = useState([]);
+  const [gameOver, setGameOver] = useState(false);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
+  useEffect(() => {
+    setGameOver(false);
+    setCurrentScore(0);
+  }, [gameOver]);
+
+  useEffect(() => {
+    if (currentScore > highScore) {
+      setHighScore(currentScore);
+    }
+  }, [currentScore, highScore]);
+
   return (
-    <div>Hello</div>
-  )
+    <div className="App">
+      <Header currentScore={currentScore} highScore={highScore} />
+      <Board
+        seen={seen}
+        setSeen={setSeen}
+        setGameOver={setGameOver}
+        currentScore={currentScore}
+        setCurrentScore={setCurrentScore}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header(props) {
+  const { currentScore, highScore } = props;
+  return (
+    <div>
+      <h1>Memory Game</h1>
+      <div>
+        <h3>Current Score: {currentScore}</h3>
+        <h3>High Score: {highScore}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
