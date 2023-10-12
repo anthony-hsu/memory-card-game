ae209663a1d9d405c5122c47a58b70a90cb6bf8c
Anthony Hsu <129446589+anthony-hsu@users.noreply.github.com>
2023-10-11 21:51:34 -0700

    Add instructions text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,18 @@ function Header(props) {
   return (
     <div>
       <h1 className="title">Memory Game</h1>
+      <div className="instructions">
+        <h4>Choose as many new emojis without repeats.</h4>
+        <h4>Try to beat the high score!</h4>
+      </div>
       <div className="score-container">
         <div className="current-score score-row">
-          <h3 style={{textAlign: "right"}}>Current Score:</h3>
-          <h3 style={{textAlign: "left"}}>{currentScore}</h3>
+          <h3 style={{ textAlign: "right" }}>Current Score:</h3>
+          <h3 style={{ textAlign: "left" }}>{currentScore}</h3>
         </div>
         <div className="high-score score-row">
-          <h3 style={{textAlign: "right"}}>High Score:</h3>
-          <h3 style={{textAlign: "left"}}>{highScore}</h3>
+          <h3 style={{ textAlign: "right" }}>High Score:</h3>
+          <h3 style={{ textAlign: "left" }}>{highScore}</h3>
         </div>
       </div>
     </div>
